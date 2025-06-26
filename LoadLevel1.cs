41bd9861bdb8549b2a8d72cee1193618fd299ccf
cnulatienpo <119638971+cnulatienpo@users.noreply.github.com>
2025-06-25 23:20:14 -0500

    Add Next button to intro scene and load level1 on click

---
+++ LoadLevel1.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel1 : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("level1");
+    }
+}
